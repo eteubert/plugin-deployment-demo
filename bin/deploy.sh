e205d10639139e540a42827c24488f8875ec8443
eteubert <eric@ericteubert.de>
2015-09-15 13:02:02 +0800

    switch to script deployment

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+echo "Starting Build $TRAVIS_TAG"
+make build
+# add build number to version
+sed -i.bak "s/\(Version:.*\)/\1.build$TRAVIS_BUILD_NUMBER/" dist/plugin-deployment-demo.php
+rm -f dist/plugin-deployment-demo.php.bak
+zip -r latest.zip dist
+curl --ftp-create-dirs -T latest.zip -u $FTP_USER:$FTP_PASSWORD ftp://eric.co.de/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
+echo "Finishing Build"
